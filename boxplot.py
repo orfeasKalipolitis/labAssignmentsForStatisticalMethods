190a4e0a1a1b1b6179e752f6307052ca15facff5
Orfeas Kalipolitis <orfeaskalipolitis@gmail.com>
2020-10-21 14:45:33 +0200

    Moved prints inside show

--- boxplot.py
+++ boxplot.py
@@ -7,8 +7,6 @@ ageMin = np.min(data.age)
 ageMedian = np.median(data.age)
 ageQ1 = np.quantile(data.age, 0.25)
 ageQ3 = np.quantile(data.age, 0.75)
-print('Q1: ', ageQ1)
-print('Q2: ', ageQ3)
 
 boxData = [data.age]
 fig3, ax3 = plt.subplots()
@@ -17,4 +15,6 @@ ax3.boxplot(boxData, showmeans=True)
 plt.xlabel('Age')
 
 def show():
+    print('Q1: ', ageQ1)
+    print('Q2: ', ageQ3)
     plt.show()
