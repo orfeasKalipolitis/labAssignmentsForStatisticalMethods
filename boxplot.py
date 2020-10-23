15baeb93d94e42c46ca15090be0b941886f189b3
Orfeas Kalipolitis <orfeaskalipolitis@gmail.com>
2020-10-23 12:25:50 +0200

    Cleaned the data before determining statistics

--- boxplot.py
+++ boxplot.py
@@ -2,19 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import dataGetter as data
 
-ageMax = np.max(data.age)
-ageMin = np.min(data.age)
-ageMedian = np.median(data.age)
-ageQ1 = np.quantile(data.age, 0.25)
-ageQ3 = np.quantile(data.age, 0.75)
+# Make a copy of the Age array
+localAge = np.array(data.age.copy())
 
-boxData = [data.age]
+# Remove 0s as they are obviously incorrect data points
+localAge = np.delete(localAge, np.argwhere(localAge == 0))
+
+ageMax = np.max(localAge)
+ageMin = np.min(localAge)
+ageMedian = np.median(localAge)
+ageQ1 = np.quantile(localAge, 0.25)
+ageQ3 = np.quantile(localAge, 0.75)
+
+boxData = [localAge]
 fig3, ax3 = plt.subplots()
 ax3.set_title('max, min, median, the first and third quartile box plot of Age')
 ax3.boxplot(boxData, showmeans=True)
 plt.xlabel('Age')
 
 def show():
-    print('Q1: ', ageQ1)
-    print('Q2: ', ageQ3)
+    print('Age max:', ageMax)
+    print('Age min:', ageMin)
+    print('Age median:', ageMedian)
+    print('Age Q1:', ageQ1)
+    print('Age Q3:', ageQ3)
     plt.show()
+    
