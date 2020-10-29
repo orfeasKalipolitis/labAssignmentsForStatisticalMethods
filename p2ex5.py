b9bf35837cee31c368dcff44ae36e9181e29048f
Orfeas Kalipolitis <orfeaskalipolitis@gmail.com>
2020-10-29 14:42:59 +0100

    Added functionality so it returns the p-values

--- p2ex5.py
+++ p2ex5.py
@@ -5,6 +5,7 @@ import numpy as np
 # dependentVar is the Y vector of the dependent variable
 # independentVars are the variables in the X matrix of Y = X * B
 # you can add as many independent variables as you want
+# returns the p-values for all independent variables
 def doTTests(dependentVar, *independentVars):
     print()
     print('Performing t-tests of each independent variable against the dependent variable:')
@@ -17,6 +18,7 @@ def doTTests(dependentVar, *independentVars):
     count = 1
     minPValue = 1
     minIndex = -1
+    pvalues = []
     for i in range(1, len(X[0])):
         print('Dependent variable #:', count)
         ttest = spStats.ttest_ind(dependentVar, X[:, i], equal_var = False)
@@ -25,8 +27,10 @@ def doTTests(dependentVar, *independentVars):
         if ttest[1] < minPValue:
             minPValue = ttest[1]
             minIndex = count
+        pvalues.append(ttest[1])
         count = count + 1
 
     print()
     print('We observe the strongest correlation between the dependent variable and independent variable #', minIndex)
     print('with a p-value of: ', minPValue)
+    return pvalues
