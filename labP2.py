3e027f91127ae73f06e86598897584d28f151b99
Orfeas Kalipolitis <orfeaskalipolitis@gmail.com>
2020-11-02 11:09:08 +0100

    Comparative bar chart

--- labP2.py
+++ labP2.py
@@ -1,6 +1,24 @@
+import dataGetterCOVID as data
 import p2ex1 as ex1
 
 print('Lab assignment - Part 2')
 print()
 print('Exercise 1: Descriptive statistics analysis for at least two qualitative and quantitative variables')
-ex1.show()
+
+# Create the data as needed
+# Get the first 25 days of data
+days = data.dataFrames[:25][:].copy()
+labels = []
+
+# Check confirmed in Abbeville and Acadia
+# Sample every 5 days
+abbevilleConfirmed = []
+acadiaConfirmed = []
+
+for dayOff in range(0, len(days), 5):
+    abbevilleConfirmed.append(days[dayOff]['Confirmed'][0])
+    acadiaConfirmed.append(days[dayOff]['Confirmed'][1])
+    labels.append('Day ' + str(data.startingDate + dayOff))
+    
+# Print a comparative bar chart between the two
+ex1.comparativeBarChart(labels, 'Confirmed Cases', abbevilleConfirmed, acadiaConfirmed, 'Abbeville', 'Acadia', 'Confirmed cases in Abbeville and Acadia')
